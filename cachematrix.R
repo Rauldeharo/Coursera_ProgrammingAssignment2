aa238dd32b264b2e9d94b1b3383c49eb6a036277
Raúl de Haro Blázquez <115423182+Rauldeharo@users.noreply.github.com>
2023-11-10 10:20:08 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object with functions to set, get, set the inverse, and get the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invr <- NULL
+    
+    ## The set function sets the matrix to a new value 'y'
+    ## and resets the stored inverse to NULL.
+    set <- function(y) {
+        x <<- y
+        invr <<- NULL
+    }
+    
+    ## The get function returns the current matrix.
+    get <- function() x
+    
+    ## The setinverse function allows setting the inverse matrix directly.
+    setinverse <- function(inverse) invr <<- inverse
+    
+    ## The getinverse function retrieves the currently stored inverse matrix.
+    getinverse <- function() invr
+    
+    ## Return a list containing the set, get, setinverse, and getinverse functions.
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+## This function calculates the inverse of a matrix using caching; caching the result for future use to avoid unnecessary recalculations.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Get the currently stored inverse matrix.
+    invr <- x$getinverse()
+    
+    ## If the inverse matrix is already cached, print a message and return it.
+    if (!is.null(invr)) {
+        message("Getting Cached Data:-")
+        return(invr)
+    }
+    
+    ## If the inverse matrix is not cached, calculate it using the solve function.
+    matrx <- x$get()
+    invr <- solve(matrx, ...)
+    
+    ## Cache the calculated inverse for future use.
+    x$setinverse(invr)
+    
+    ## Return the calculated inverse.
+    invr
 }
+
